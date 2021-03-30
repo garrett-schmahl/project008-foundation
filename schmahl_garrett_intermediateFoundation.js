d8ea76903562c1b3cc7e6e4d0fe00f55278360b5
Garrett Schmahl <gschmahl@pm.me>
2021-03-30 14:43:22 -0700

    fix problem 7 to use splice

--- schmahl_garrett_intermediateFoundation.js
+++ schmahl_garrett_intermediateFoundation.js
@@ -110,13 +110,11 @@ console.log(secondLargest([1,2]))
 // 7. Double Trouble: Create a function that changes a given array to list each existing element twice, retaining original order.  Convert [4, "Ulysses", 42, false] to [4,4, "Ulysses", "Ulysses", 42, 42, false, false].
 
 function doubleTrouble(x){
-  tempArray = []
-  for( let i = 0; i< x.length; i++){
-    for(let j = 0; j < 2; j++){
-      tempArray.push(x[i])
-    }
+  for( let i = 0; i < x.length; i++){
+    var temp = arr[i]
+    x.splice(i, 0, temp)
+    i++
   }
-  x = tempArray
   return x
 }
 let tsa2 = tsaMaster.slice()
